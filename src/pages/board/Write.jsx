96fc5b15ccc8f3627f11b45636cb325b72f5673c
seonghun724 <seonghun724@gmail.com>
2024-10-10 15:03:32 +0900

    게시글 작성 후 로컬 json에 데이터 넣는 기능

--- src/pages/board/Write.jsx
+++ src/pages/board/Write.jsx
@@ -1,26 +1,27 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom'; // 페이지 이동을 위한 useNavigate
 import BoardForm from '../../components/Board/BoardForm';
+import axios from 'axios';
 
 const Write = () => {
+  const navigate = useNavigate(); // useNavigate 훅 사용
+
   const handleSubmit = async formData => {
     try {
-      const response = await fetch('http://localhost:3000/boards', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(formData),
-      });
+      const response = await axios.post(
+        'http://localhost:3001/boards',
+        formData
+      );
 
-      if (!response.ok) {
+      if (response.status !== 201) {
         throw new Error('서버에서 에러가 발생했습니다.');
       }
 
-      const result = await response.json();
-      console.log('게시글 작성 완료:', result);
-      // 성공 시 다른 페이지로 이동하거나 알림 메시지를 보여줄 수 있습니다.
+      alert('게시글 작성이 완료되었습니다.');
+      navigate('/board/List'); // 게시글 메인 페이지로 이동
     } catch (error) {
       console.error('게시글 작성 중 오류 발생:', error);
+      alert('게시글 작성에 실패했습니다. 다시 시도해주세요.');
     }
   };
 
