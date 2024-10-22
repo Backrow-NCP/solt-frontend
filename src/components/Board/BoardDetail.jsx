7f11226eaf492d850f8edb95d53cb0e3a95f2d0e
seonghun724 <seonghun724@gmail.com>
2024-10-22 21:50:13 +0900

    게시글 수정 작업 중

--- src/components/Board/BoardDetail.jsx
+++ src/components/Board/BoardDetail.jsx
@@ -1,4 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom'; // useNavigate import 추가
+import { getMemberId } from '../../utils/token/tokenUtils';
 import {
   InfoTextTitleContainer,
   DateAuthorContainer,
@@ -18,11 +20,35 @@ import {
 
 import PrevButton from '../../assets/images/prevButton.svg';
 import NextButton from '../../assets/images/nextButton.svg';
-import axios from 'axios'; // axios를 가져옵니다.
+
 import ReplySection from './ReplySection'; // ReplySection을 가져옵니다.
+import defaultImage from '../../assets/images/sample/nonImage.jpg';
+import apiClient, { setupInterceptors } from '../../config/AxiosConfig';
 
 const BoardDetail = ({ boardData }) => {
+  console.log('나 겟멤버 함수', getMemberId());
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const [loading, setLoading] = useState(false);
+
+  const navigate = useNavigate(); // navigate 변수를 정의합니다.
+  useEffect(() => {
+    setupInterceptors(setLoading);
+
+    const handleKeyDown = event => {
+      if (event.key === 'ArrowRight') {
+        handleNextImage();
+      } else if (event.key === 'ArrowLeft') {
+        handlePrevImage();
+      }
+    };
+
+    window.addEventListener('keydown', handleKeyDown);
+
+    // 컴포넌트가 언마운트될 때 이벤트 리스너 제거
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [currentImageIndex, boardData]);
 
   const handleNextImage = () => {
     if (boardData) {
@@ -45,10 +71,18 @@ const BoardDetail = ({ boardData }) => {
     setCurrentImageIndex(index);
   };
 
+  console.log('');
   const handleEdit = async () => {
     try {
-      const response = await axios.get(`/board/edit/${boardData.boardId}`);
-      console.log('Edit Response:', response.data);
+      navigate(`/board/edit/${boardData.boardId}`, {
+        state: {
+          content: boardData.content,
+          title: boardData.title,
+          fileName: boardData.fileName,
+          plan: boardData.plan,
+          memberId: boardData.member.memberId,
+        },
+      }); // navigate 호출 종료
     } catch (error) {
       console.error('Error fetching edit data:', error);
     }
@@ -56,43 +90,74 @@ const BoardDetail = ({ boardData }) => {
 
   const handleDelete = async () => {
     try {
-      const response = await axios.delete(`board/delete/${boardData.boardId}`);
+      const response = await apiClient.delete(
+        `board/delete/${boardData.boardId}`
+      );
       console.log('Delete Response:', response.data);
     } catch (error) {
       console.error('Error deleting the post:', error);
     }
   };
 
+  const thumbnailize = '?type=f&w=50&h=50'; // 썸네일 크기 설정
+  const imageUrl =
+    boardData.images?.length > 0
+      ? `${process.env.REACT_APP_IMAGE_STORAGE_URL}${boardData.images[currentImageIndex].fileName}`
+      : defaultImage; // 기본 이미지 설정
+
   if (!boardData) {
     return console.log('!boardData일때, 보드디테일', boardData);
   }
   console.log('보드데이터 체크용, 보드디테일', boardData);
 
+  const calculateDuration = (startDate, endDate) => {
+    const start = new Date(startDate);
+    const end = new Date(endDate);
+
+    const dayInMilliseconds = 24 * 60 * 60 * 1000;
+
+    // 두 날짜 차이 계산
+    const diffInMilliseconds = end - start;
+    const diffInDays = Math.round(diffInMilliseconds / dayInMilliseconds);
+
+    if (diffInDays === 0) {
+      return '당일치기';
+    }
+
+    const nights = diffInDays; // 숙박 일수
+    const days = nights + 1; // 총 일수
+
+    return `${nights}박 ${days}일`;
+  };
+  const currentMemberId = getMemberId();
   return (
     <>
       <BoardContainer>
         <InfoTextTitleContainer>
           <InfoText>
-            [{boardData.location}] [{boardData.duration}]
+            [{boardData.plan.location}] [
+            {calculateDuration(
+              boardData.plan.startDate,
+              boardData.plan.endDate
+            )}
+            ]
           </InfoText>
           <Title>{boardData.title}</Title>
         </InfoTextTitleContainer>
         <DateAuthorContainer>
-          {new Date(boardData.regDate).toLocaleString()} /{' '}
-          {boardData.member.name} 님
+          {new Date(boardData.regDate).toLocaleDateString()}
+          {new Date(boardData.regDate).toLocaleTimeString([], {
+            hour: '2-digit',
+            minute: '2-digit',
+          })}{' '}
+          / {boardData.member.name} 님
         </DateAuthorContainer>
 
         <ButtonContainer>
           <Button onClick={handlePrevImage}>
             <img src={PrevButton} alt="이전 이미지" />
           </Button>
-          <Image
-            src={
-              boardData.images[currentImageIndex]?.fileName ||
-              '/sampleImage/nonImage.jpg'
-            }
-            alt="게시물 이미지"
-          />
+          <Image src={imageUrl} alt="게시물 이미지" />
           <Button onClick={handleNextImage}>
             <img src={NextButton} alt="다음 이미지" />
           </Button>
@@ -102,7 +167,7 @@ const BoardDetail = ({ boardData }) => {
           {boardData.images.map((image, index) => (
             <Thumbnail
               key={image.fileName}
-              src={image.fileName}
+              src={`${process.env.REACT_APP_IMAGE_STORAGE_URL}${image.fileName}${thumbnailize}`} // 썸네일 URL 생성
               alt={`썸네일 ${index + 1}`}
               className={currentImageIndex === index ? 'active' : ''}
               onClick={() => handleThumbnailClick(index)}
@@ -112,11 +177,12 @@ const BoardDetail = ({ boardData }) => {
 
         <Content>{boardData.content}</Content>
 
-        <ButtonContainerStyled>
-          <EditButton onClick={handleEdit}>수정</EditButton>
-          <DeleteButton onClick={handleDelete}>삭제</DeleteButton>
-        </ButtonContainerStyled>
-
+        {currentMemberId === boardData.member.memberId && (
+          <ButtonContainerStyled>
+            <EditButton onClick={handleEdit}>수정</EditButton>
+            <DeleteButton onClick={handleDelete}>삭제</DeleteButton>
+          </ButtonContainerStyled>
+        )}
         {/* ReplySection 컴포넌트 삽입 */}
         <ReplySection boardId={boardData.boardId} />
       </BoardContainer>
--- src/pages/board/Edit.jsx
+++ src/pages/board/Edit.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import BoardForm from '../../components/Board/BoardForm';
+import { useLocation, useNavigate } from 'react-router-dom';
+import apiClient, { setupInterceptors } from '../../config/AxiosConfig';
 
 const Edit = () => {
   const handleSubmit = e => {
     e.preventDefault();
+
     console.log('게시글 수정 완료');
   };
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const { content, title, fileName, plan, memberId } = location.state || {};
+
+  console.log('수정페이지 데이터', plan);
+  console.log('수정페이지 데이터', memberId);
 
   return (
     <div className="inner">
@@ -23,3 +33,10 @@ const Edit = () => {
 };
 
 export default Edit;
+
+// 해야하는 것: navigate로 넘어왔을 때 기본 값들을 어떻게 렌더링 할것인지
+// => boardForm에 데이터를 넘겨주고 섹션마다 value를 두어서 게시글 메인에서 작성페이지로 넘어왔다면
+// 아무런 렌더링 안되고 게시글 조회 중 수정페이지로 이동한다면 value값들이 다 들어있는 형식으로
+
+// 제출 시 넘겨줘야할 데이터들? 인가? => 이 경우엔 boardData를 수정페이지에서 전부 받고 제출 시 다시 전부 줘야 함
+// 제출 시 수정해야할 데이터들? 인가? => 이 경우에는 데이터를 굳이 넘겨주지 않아도 저절로 렌더링 됨.
